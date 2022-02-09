95f09a9ebc3fdb8be883bfe9f68d2f3808e4864e
yeseniav32 <95379817+yeseniav32@users.noreply.github.com>
2022-02-08 19:28:56 -0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os 
+
+basedir=os.path.abspath(os.path.dirname(__file__))
+
+class Config():
+    SECRETY_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False #Turn off update messages from sql
